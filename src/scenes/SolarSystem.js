6692af504f7bc4677057f83fa59b26add9be6721
Vinko Azinovic <vinko.azinovic@gmail.com>
2022-12-30 20:39:50 +0100

    add skybox

--- src/scenes/SolarSystem.js
+++ src/scenes/SolarSystem.js
@@ -9,6 +9,7 @@ import {
   StandardMaterial,
   Color3,
   Texture,
+  CubeTexture,
 } from '@babylonjs/core';
 
 const createScene = (canvas) => {
@@ -17,6 +18,8 @@ const createScene = (canvas) => {
   scene.clearColor = new Color3(0, 0, 0); // Change scene background color
   const camera = new ArcRotateCamera('camera', 0, 0, 15, Vector3.Zero(), scene); // Create camera
   camera.attachControl(canvas); // Let the user move the camera
+  //  camera.upperRadiusLimit = 50;
+  //  camera.maxZ = 30;
   const light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene); // Create light
   light.intensity = 0.5;
   light.groundColor = new Color3(0, 0, 1);
@@ -39,6 +42,17 @@ const createScene = (canvas) => {
   const planet1 = MeshBuilder.CreateSphere('planet1', { segments: 16, diameter: 1 }, scene);
   planet1.material = planet1Material;
   planet1.position.x = 4;
+
+  // Create skybox
+  const skybox = new MeshBuilder.CreateBox('skybox', { size: 1000.0 }, scene);
+  //  skybox.infiniteDistance = true;
+  const skyBoxMaterial = new StandardMaterial('skybox', scene);
+  skyBoxMaterial.backFaceCulling = false; // Don't render what we cant see
+  skyBoxMaterial.reflectionTexture = new CubeTexture('/assets/skybox', scene);
+  skyBoxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+  skyBoxMaterial.specularColor = new Color3(0, 0, 0);
+  skyBoxMaterial.diffuseColor = new Color3(0, 0, 0);
+  skybox.material = skyBoxMaterial;
 
   engine.runRenderLoop(() => {
     scene.render();
